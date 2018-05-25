4ca2e3132ac067b2ad7b3b502bdf2110a9704305
Brayan Henao <bryanhenao96@gmail.com>
2018-05-24 22:30:08 -0500

    Update app.py

--- A00056004/op_stats/app.py
+++ A00056004/op_stats/app.py
@@ -4,10 +4,23 @@ from op_stats.stats import Stats
 
 app = Flask(__name__)
 
-@app.route('/v1/stats/cpu')
+@app.route('/getStats/cpu')
 def get_cpuinfo():
     cpu_percent = Stats.get_cpu_percent()
-    return json.dumps({'cpu_percent': cpu_percent})
+    return json.dumps({'Cpu usage': cpu_percent})
+
+
+@app.route('/getStats/ram')
+def get_raminfo():
+    ram = Stats.get_ram()
+    return json.dumps({'Ram available': ram})
+
+
+@app.route('/getStats/disk')
+def get_diskinfo():
+    free_disk = Stats.get_free_disk()
+    return json.dumps({'Free disk': free_disk})
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=8080)
