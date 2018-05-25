a7de1a41033c29a09a3b88e6b782f9862eff8926
Brayan Henao <bryanhenao96@gmail.com>
2018-05-24 22:26:07 -0500

    Update stats.py

--- A00056004/op_stats/stats.py
+++ A00056004/op_stats/stats.py
@@ -6,3 +6,15 @@ class Stats():
   def get_cpu_percent(cls):
     cpu_percent = psutil.cpu_percent()
     return cpu_percent
+	
+  @classmethod
+  def get_ram(cls):
+    memory = psutil.virtual_memory();
+    available = memory[1]
+    return available
+  
+  @classmethod
+  def get_free_disk(cls):
+    disk = psutil.disk_usage('/')
+	free_disk = disk[2]
+    return free_disk
